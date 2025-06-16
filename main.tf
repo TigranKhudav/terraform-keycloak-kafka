e9623a3e68005cffe289c80e0dbbf683263b43fe
Tigran Khudaverdyan <tigran@Tigrans-MacBook-Pro.local>
2025-06-16 19:34:31 +0400

    Uncomment policies assignment in consumer authorization permission for proper role association

--- main.tf
+++ main.tf
@@ -48,8 +48,8 @@ resource "keycloak_openid_client_authorization_permission" "consumer" {
   resource_server_id = var.client_id
   name               = "consumer-${var.topic_name}"
   type               = "scope"
-  policies           = [keycloak_openid_client_role_policy.this["consumer"].id]
-  # resources          = [keycloak_openid_client_authorization_resource.this.id]
+  # policies           = [keycloak_openid_client_role_policy.this["consumer"].id]
+  resources          = [keycloak_openid_client_authorization_resource.this.id]
   scopes             = var.scopes
-  # depends_on         = [keycloak_openid_client_authorization_resource.this, keycloak_openid_client_role_policy.this]
+  depends_on         = [keycloak_openid_client_authorization_resource.this, keycloak_openid_client_role_policy.this]
 }
