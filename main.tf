e859c9b48bf12213dc91a07cb673065b5f4aa679
Tigran Khudaverdyan <tigran@Tigrans-MacBook-Pro.local>
2025-06-16 18:09:23 +0400

    Format scopes in Keycloak authorization resource for consistency

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "keycloak_openid_client_authorization_resource" "this" {
   resource_server_id  = var.client_id
   name                = "Topic:${var.topic_name}"
   display_name        = "Topic:${var.topic_name}"
-  scopes              = ["Write","Describe","Read"]
+  scopes              = ["Write", "Describe", "Read"]
   type                = "Topic"
 }
 
@@ -32,14 +32,14 @@ resource "keycloak_openid_client_role_policy" "this" {
   depends_on      = [keycloak_role.this]
 }
 
-resource "keycloak_openid_client_authorization_permission" "this" {
-  for_each           = toset(local.roles)
-  realm_id           = var.realm_id
-  resource_server_id = var.client_id
-  name               = "${each.key}-${var.topic_name}"
-  type               = "scope"
-  policies           = [keycloak_openid_client_role_policy.this[each.key].id]
-  resources          = [keycloak_openid_client_authorization_resource.this.id]
-  scopes             = var.scopes
-  depends_on         = [keycloak_openid_client_authorization_resource.this, keycloak_openid_client_role_policy.this]
-}
+# resource "keycloak_openid_client_authorization_permission" "this" {
+#   for_each           = toset(local.roles)
+#   realm_id           = var.realm_id
+#   resource_server_id = var.client_id
+#   name               = "${each.key}-${var.topic_name}"
+#   type               = "scope"
+#   policies           = [keycloak_openid_client_role_policy.this[each.key].id]
+#   resources          = [keycloak_openid_client_authorization_resource.this.id]
+#   scopes             = var.scopes
+#   depends_on         = [keycloak_openid_client_authorization_resource.this, keycloak_openid_client_role_policy.this]
+# }
