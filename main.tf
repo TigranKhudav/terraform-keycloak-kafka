dff18a19b0d98977a157e910c4124e4981c27aa7
Tigran Khudaverdyan <tigran@Tigrans-MacBook-Pro.local>
2025-06-16 14:45:47 +0400

    Refactor Keycloak authorization scopes to use static values for improved clarity and simplicity

--- main.tf
+++ main.tf
@@ -1,28 +1,13 @@
 locals {
   roles = ["producer", "consumer"]
 }
-data "keycloak_openid_client_authorization_scope" "describe" {
-  realm_id           = var.realm_id
-  resource_server_id = var.client_id
-  name               = "Describe"
-}
-data "keycloak_openid_client_authorization_scope" "write" {
-  realm_id           = var.realm_id
-  resource_server_id = var.client_id
-  name               = "Write"
-}
-data "keycloak_openid_client_authorization_scope" "read" {
-  realm_id           = var.realm_id
-  resource_server_id = var.client_id
-  name               = "Read"
-}
 
 resource "keycloak_openid_client_authorization_resource" "this" {
   realm_id            = var.realm_id
   resource_server_id  = var.client_id
   name                = "Topic:${var.topic_name}"
   display_name        = "Topic:${var.topic_name}"
-  scopes              = [data.keycloak_openid_client_authorization_scope.describe.id, data.keycloak_openid_client_authorization_scope.write.name, data.keycloak_openid_client_authorization_scope.read.name]
+  scopes              = ["Write","Describe","Read"]
   type                = "Topic"
 }
 
@@ -55,9 +40,6 @@ resource "keycloak_openid_client_authorization_permission" "this" {
   type               = "scope"
   policies           = [keycloak_openid_client_role_policy.this[each.key].id]
   resources          = [keycloak_openid_client_authorization_resource.this.id]
-  scopes = [
-    data.keycloak_openid_client_authorization_scope.describe.id,
-    each.key == "producer" ? data.keycloak_openid_client_authorization_scope.write.id : data.keycloak_openid_client_authorization_scope.read.id
-  ]
+  scopes             = var.scopes
   depends_on         = [keycloak_openid_client_authorization_resource.this, keycloak_openid_client_role_policy.this]
 }
